0a42bb35554300cdf582194f186627f369ed9326
tanveerisonline <tanveerisonline@gmail.com>
2022-11-17 19:36:39 +0530

    Add css to form validation

--- form.js
+++ form.js
@@ -1,11 +1,12 @@
-const email = document.querySelector('#email-id')
-const submit = document.querySelector('.submit-button')
-const emailError = document.querySelector('.error')
+const email = document.querySelector("#email-id");
+const submit = document.querySelector(".submit-button");
+const emailError = document.querySelector(".error");
 
-submit.addEventListener('click', () => {
-    if (email.validity.typeMismatch) {
-        emailError.textContent = 'Enter a valid email address';
-    } else {
-        emailError.textContent = '';
-    }
-})
+submit.addEventListener("click", () => {
+  if (email.validity.typeMismatch) {
+    emailError.textContent =
+      "please enter a valid email id or write your email id in lower case";
+  } else {
+    emailError.textContent = "";
+  }
+});
